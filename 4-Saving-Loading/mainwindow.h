e12b67ed87e80e4cf797aaa49ec039e8168c0fe3
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-12-22 20:09:48 +0530

    Saving and Loading (Code)

---
+++ 4-Saving-Loading/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+ 
+#include <KXmlGuiWindow>
+
+class KTextEdit;
+class KJob;
+ 
+class MainWindow : public KXmlGuiWindow
+{
+    Q_OBJECT
+    
+  public:
+    MainWindow(QWidget *parent=0);
+ 
+  private:
+    KTextEdit* textArea;
+    void setupActions();
+    
+    QString fileName;
+ 
+  private slots:
+    void newFile();
+    void openFile();
+    void saveFile();
+    void saveFileAs();
+    void saveFileAs(const QString &outputFileName);
+    
+    void downloadFinished(KJob* job);
+};
+ 
+#endif
